7af239560c3a9f3680caf1a6344c2052861a551b
AliAtherAyyubi <info2atherayyubi@gmail.com>
2025-01-23 13:13:36 +0500

    removed

--- Python/AI Algo/panda.py
+++
@@ -1,9 +0,0 @@
-
-
-import pandas as pd
-
-# file=pd.read_csv('GOOGLE.csv')
-file=pd.read_csv('GOOGLE.csv',names=['Date','Open'])
-# file=pd.read_csv('info.txt')
-
-print(file)
---
+++ Python/leetCode1.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        index=[]
+        print(nums[1])
+        
+        # for i in nums:
+        #     if nums[i]+nums[i+1]==target:
+        #         index
+        #     else if 
+        
+solution= Solution()
+result=solution.twoSum([1,2,3],3)
+print(result)
